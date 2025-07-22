5162fb3dee8a5da472fb29412ef13fc431a3589e
Ekaterina-RA <ekaterina.kuz@gmail.com>
2025-07-22 16:40:27 +0300

    Update serializers.py
    Обновлены сериализаторы курсов и уроков

--- onlinelearning/serializers.py
+++ onlinelearning/serializers.py
@@ -1,21 +1,38 @@
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Course, Lesson
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class LessonListSerializer(serializers.ModelSerializer):
+    """Сериализатор для списка уроков"""
     class Meta:
         model = Lesson
-        fields = ["id", "title", "description", "video_link", "course"]
+        fields = ['id', 'title', 'description', 'video_link']
+
+
+class LessonDetailSerializer(serializers.ModelSerializer):
+    """Сериализатор для детального просмотра урока"""
+    class Meta:
+        model = Lesson
+        fields = '__all__'
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    lesson_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(source="lessons.all", many=True, read_only=True)
+    lessons = LessonListSerializer(
+        source='lessons.all', 
+        many=True, 
+        read_only=True,
+        help_text="Список уроков курса"
+    )
+    lesson_count = serializers.SerializerMethodField(
+        help_text="Количество уроков в курсе"
+    )
 
     class Meta:
         model = Course
-        fields = ["id", "title", "description", "lesson_count", "lessons"]
+        fields = ['id', 'title', 'description', 'preview', 'lesson_count', 'lessons']
+        extra_kwargs = {
+            'preview': {'read_only': True}
+        }
 
     def get_lesson_count(self, obj):
         return obj.lessons.count()
