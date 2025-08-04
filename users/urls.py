5be33154019dc87e298c5763bb6ab7f0a6041366
Ekaterina-RA <ekaterina.kuz@gmail.com>
2025-08-04 10:29:08 +0300

    Update users/urls.py
    Urls for PaymentViewSet is added in the users' app, since model Payment is described in this app:
    router.register(r"payments", PaymentViewSet, basename="payments")

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
-
+from users.views import PaymentViewSet
 from .apps import UsersConfig
 from .views import CustomTokenObtainPairView, UserViewSet
 
@@ -9,10 +9,11 @@ app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
-
+router.register(r"payments", PaymentViewSet, basename="payments")
 
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    
 ]
